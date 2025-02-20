ad0a8d00ca05d80ca87660bb669d9bb7cbc269ca
SangGuk Lee <nagazam21@naver.com>
2025-02-21 01:34:04 +0900

    Update axios.ts
    base url chg
    https

--- krampoline/src/apis/axios.ts
+++ krampoline/src/apis/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://goormtago.qook.io/",
+  baseURL: "https://goormtago.qook.io/",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
